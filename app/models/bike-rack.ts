5eae7f7a85914737928a4e5c94b0ef9aabcb64b8
unknown <brendananderson62902@gmail.com>
2016-02-02 11:28:09 -0600

    Small loan of a million dollars

--- app/models/bike-rack.ts
+++ app/models/bike-rack.ts
@@ -1,12 +1,13 @@
 export class BikeRack {
   id: number;
   name: string;
-
+latitude: string;
   constructor(
     obj: any
   ) {
     this.id = obj.id;
     this.name = obj.name;
+    this.latitude = obj.latitude;
   }
 
 }
