f0ca40e4d9070e6a063428ed56dca4f30e6aa17b
Cory Dingels <dingels35@gmail.com>
2016-02-21 11:00:42 -0600

    show thank you after logging in

--- app/pages/log-in/log-in.ts
+++ app/pages/log-in/log-in.ts
@@ -1,7 +1,8 @@
 import {AuthHttp} from 'angular2-jwt/angular2-jwt';
-import {Page} from 'ionic-framework/ionic';
+import {Page, NavController} from 'ionic-framework/ionic';
 import {WybNavbar} from '../../components/wyb-navbar';
 import {AuthorizationService} from '../../services/authorization-service'
+import {GettingStartedPage} from '../getting-started/getting-started';
 
 @Page({
   templateUrl: 'build/pages/log-in/log-in.html',
@@ -10,14 +11,20 @@ import {AuthorizationService} from '../../services/authorization-service'
 })
 export class LogInPage {
   lock: Auth0Lock = new Auth0Lock('nlCX5QpwjRbWT5ATwDigu0kwsTikjha0', 'willmaryellowbikes.auth0.com');
+  public hasLoggedIn: boolean = false;
 
-  constructor(public auth: AuthorizationService) { }
+  constructor(public auth: AuthorizationService, public nav: NavController) { }
+
+  goHome() {
+    this.nav.setRoot(GettingStartedPage);
+  }
 
   logIn() {
-    let auth = this.auth; // need to reference this inside callback funtion
+    let vm = this;
 
     this.lock.show(function(err, profile, id_token) {
       if(err) {
+        // TODO: show an error message
         throw new Error(err);
       }
 
@@ -25,11 +32,13 @@ export class LogInPage {
       console.log(profile);
       console.log(id_token);
 
+      // save creds
       localStorage.setItem('profile', JSON.stringify(profile));
       localStorage.setItem('id_token', id_token);
-      auth.setActivePages();
+      vm.auth.setActivePages();
 
-      // TODO: show a thank you for logging in page
+      // show thank you
+      vm.hasLoggedIn = true;     
 
     });
   }
